bf1a831c8e6acb865ade252364be6ce9e7fd91bf
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2011-06-12 22:00:19 +0000

    change name

--- scrapers/twitpic-launcher.sh
+++ scrapers/twitpic-launcher.sh
@@ -2,5 +2,5 @@
 
 for i in "acampadasol" "spanishrevolution" "democraciarealya" "15menpleno" "nolesvotes" "acampadaacoruna" "acampadacoruna" "acampadaalcala" "acampadaalicante" "acampadaalmeria" "acampadabarcelona" "acampadabcn" "acampadabenicarlo" "acampadabilbao" "acampadacadiz" "acampadacaceres" "acampadacastellon" "acampadachiclana" "acampadacordoba" "acampadadonostia" "acampadaelche" "acampadaferrol" "acampadagasteiz" "acampadagranada" "acampadaibiza" "acampadajerez" "acampadalaspalmas" "acampadapalmas" "acampadaleon" "acampadalleida" "acampadalogrono" "acampadamadrid" "acampadamalaga" "acampadamurcia" "acampadapalencia" "acampadapalma" "acampadamallorca" "acampadapamplona" "acampadaoviedo" "acampadaronda" "acampadasalamanca" "acampadasantiago" "acampadasevilla" "acampadasoria" "acampadatarragona" "acampadaterrasa" "acampadatoledo" "acampadavalencia" "acampadavigo" "acampadavlc" "acampadazamora" "acampadazaragoza" "acampadazgz"
 do
-    python twitpicimages.py $i &
+    python twitpic.py $i &
 done
