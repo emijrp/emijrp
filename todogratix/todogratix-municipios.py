5e3319ced4cab4c8a5b72ba77765e34a147225c1
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2012-03-03 20:17:45 +0000

    municipios

---
+++ todogratix/todogratix-municipios.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+import catlib
+import pagegenerators
+import wikipedia
+
+def formatnum(n):
+    n = re.sub(ur",", ur"", n)
+    n = re.sub(ur"\.", ur",", n)
+    return n
+
+sitetg = wikipedia.Site('todogratix', 'todogratix')
+sitees = wikipedia.Site('es', 'wikipedia')
+category = catlib.Category(title=u"Categoría:Municipios de la provincia de Almería", site=sitees)
+gen = pagegenerators.CategorizedPageGenerator(category, recurse=False, start=sys.argv[1])
+pre = pagegenerators.PreloadingGenerator(gen)
+
+#load poblacion
+pagepob = wikipedia.Page(sitees, u"Plantilla:Población municipios de España")
+m = re.findall(ur"(?im)\|(\d+)=(\d+)", pagepob.get())
+pob = {}
+for i in m:
+    pob[i[0]] = re.sub(ur"[,\.]", ur"", i[1])
+    #print i[0], i[1]
+
+for page in pre:
+    if page.exists() and not page.isRedirectPage() and page.namespace() == 0 and re.search(ur"\{\{\s*Ficha de localidad de España", page.get()):
+        print page.title()
+        props = {'title': page.title(), 'nombre': '', 'escudo': '', 'bandera': '', 'imagen': '', 'pie': '', 'cod_provincia': '', 'cod_municipio': '', 'coord': '', 'localizacion': '', 'altitud': '', 'superficie': '', 'pob': '', 'anyopob': '', 'gentilicio': '', 'cp': '', 'alcalde': '', 'partido': '', 'patron': '', 'patrona': '', 'web': '', 'latd': 0, 'latm': 0, 'lats': 0, 'latns': '', 'longd': 0, 'longm': 0, 'longs': 0, 'longew': ''}
+        m = re.findall(ur"(?im)\|\s*(cod[ _]provincia|cod[ _]municipio|escudo|bandera|nombre|imagen|pie[ _]de[ _]imagen|superficie|altitud|gentilicio|cp|alcalde|patr[óo]n|patrona|sitio[ _]web|web|latd|latm|latNS|longd|longm|longEW)\s*=\s*([^\n\r\|]+)\s*[\n\r\|]", page.get())
+        for i in m:
+            if i[0].lower() == 'nombre' and not props['nombre']:
+                props['nombre'] = i[1].strip()
+            elif i[0].lower() == 'escudo' and not props['escudo']:
+                props['escudo'] = i[1].strip()
+            elif i[0].lower() == 'bandera' and not props['bandera']:
+                props['bandera'] = i[1].strip()
+            elif i[0].lower() == 'imagen' and not props['imagen']:
+                if re.search(ur"(?:Archivo|Imagen?|File)", i[1]):
+                    props['imagen'] = re.sub(ur"(?im)^.*(?:Archivo|Imagen?|File)\s*\:\s*([^\|]+)\s*[\|].*$", ur"\1", i[1].strip())
+                else:
+                    props['imagen'] = ''
+            elif i[0].lower() in ['pie de imagen', 'pie_de_image'] and not props['pie']:
+                props['pie'] = i[1].strip()
+            elif i[0].lower() in ['cod_provincia', 'cod provincia'] and not props['cod_provincia']:
+                props['cod_provincia'] = i[1].strip()
+            elif i[0].lower() in ['cod_municipio', 'cod municipio'] and not props['cod_municipio']:
+                props['cod_municipio'] = i[1].strip()
+            elif i[0].lower() == 'superficie' and not props['superficie']:
+                props['superficie'] = formatnum(i[1].strip())
+            elif i[0].lower() == 'altitud' and not props['altitud']:
+                props['altitud'] = formatnum(i[1].strip())
+            elif i[0].lower() == 'gentilicio' and not props['gentilicio']:
+                props['gentilicio'] = i[1].split(',')[0].strip().split('<')[0].strip()
+            elif i[0].lower() == 'cp' and not props['cp']:
+                props['cp'] = i[1].strip()
+                if re.search(ur" ", props['cp']):
+                    props['cp'] = ''
+            elif i[0].lower() in [u'patrón', u'patron'] and not props['patron']:
+                t = re.sub(ur"\[\[([^\|\]]+)\|[^\]]+\]\]", ur"\1", i[1])
+                t = re.sub(ur"[\[\]]", ur"", t)
+                props['patron'] = t.strip()
+            elif i[0].lower() == 'patrona' and not props['patrona']:
+                t = re.sub(ur"\[\[([^\|\]]+)\|[^\]]+\]\]", ur"\1", i[1])
+                t = re.sub(ur"[\[\]]", ur"", t)
+                props['patrona'] = t.strip()
+            elif i[0].lower() == 'alcalde' and not props['alcalde'] and not props['partido']:
+                t = re.sub(ur"\[\[([^\|\]]+)\|[^\]]+\]\]", ur"\1", i[1])
+                t = re.sub(ur"[\[\]]", ur"", t)
+                if re.search(ur"\(", t):
+                    props['alcalde'] = t.split('(')[0].strip()
+                    props['partido'] = t.split('(')[1].strip()
+                    if props['partido'][-1] == ')':
+                        props['partido'] = props['partido'][:-1]
+                else:
+                    props['alcalde'] = t.strip()
+                    props['partido'] = ''
+                if props['partido'] == 'IU':
+                    props['partido'] = 'Izquierda Unida'
+                elif props['partido'] == 'PP':
+                    props['partido'] = 'Partido Popular'
+                elif props['partido'] == 'PSOE':
+                    props['partido'] = u'Partido Socialista Obrero Español'
+            elif i[0].lower() in ['web', 'sitio_web', 'sitio web'] and not props['web']:
+                props['web'] = re.findall(ur"(https?://[^/ ]+)", i[1])[0].strip()
+            elif i[0].lower() == 'latd' and not props['latd']:
+                props['latd'] = i[1].strip()
+            elif i[0].lower() == 'latm' and not props['latm']:
+                props['latm'] = i[1].strip()
+            elif i[0].lower() == 'lats' and not props['lats']:
+                props['lats'] = i[1].strip()
+            elif i[0].lower() == 'latns' and not props['latns']:
+                props['latns'] = i[1].strip()
+            elif i[0].lower() == 'longd' and not props['longd']:
+                props['longd'] = i[1].strip()
+            elif i[0].lower() == 'longm' and not props['longm']:
+                props['longm'] = i[1].strip()
+            elif i[0].lower() == 'longs' and not props['longs']:
+                props['longs'] = i[1].strip()
+            elif i[0].lower() == 'longew' and not props['longew']:
+                props['longew'] = i[1].strip()
+            
+        #localización
+        props['localizacion'] = [u"Provincia de Almería", u"Andalucía", u"España"]
+        if props['cod_provincia'] and props['cod_municipio']:
+            props['pob'] = pob['%s%s' % (props['cod_provincia'], props['cod_municipio'])]
+        else:
+            print props['cod_provincia'], props['cod_municipio']
+            sys.exit()
+        props['anyopob'] = 2011
+        props['coord'] = '%f, %f'% (abs(float(props['latd'])+float(props['latm'])/60+float(props['lats'])/3600)*int((props['latns'].lower() in ['n', ''] and '1' or '-1')), abs(float(props['longd'])+float(props['longm'])/60+float(props['longs'])/3600)*int((props['longew'].lower() in ['e', ''] and '1' or '-1')))
+        
+        print props.items()
+        output = u"""{{Infobox Ciudad
+|nombre=%s
+|escudo=%s
+|bandera=%s
+|imagen=%s
+|pie de imagen=%s
+|coordenadas=%s
+|localización=%s
+|altitud=%s
+|superficie=%s
+|población=%s
+|año de población=%s
+|gentilicio=%s
+|código postal=%s
+|alcalde=%s
+|partido del alcalde=%s
+|patrón=%s
+|patrona=%s
+|web=%s
+}}
+== Enlaces externos ==
+* {{w|es|%s}}
+""" % (props['nombre'], props['escudo'], props['bandera'], props['imagen'], props['pie'], props['coord'], ', '.join(props['localizacion']), props['altitud'], props['superficie'], props['pob'], props['anyopob'], props['gentilicio'], props['cp'], props['alcalde'], props['partido'], props['patron'], props['patrona'], props['web'], props['title'])
+        print output
+        
+        tgpage = wikipedia.Page(sitetg, props['title'])
+        if not tgpage.exists():
+            tgpage.put(output, u"BOT - Importando metadatos desde Wikipedia en español http://es.wikipedia.org/wiki/%s" % (re.sub(ur" ", ur"_", props['title'])))
--- todogratix/todogratix-openlibrary-books.py
+++ todogratix/todogratix-openlibrary-books.py
@@ -8,11 +8,11 @@ import urllib
 import wikipedia
 
 site = wikipedia.Site('todogratix', 'todogratix')
-f = open('books.spa', 'r')
+f = open('booksall.spa', 'r')
 books = f.read().splitlines()
 f.close()
 
-skip = u'/b/OL22886366M'
+skip = u'/b/OL14006205M'
 for book in books:
     print book
     if skip:
@@ -45,11 +45,76 @@ for book in books:
         data2 = json.load(json_data2)
         if data2.has_key('name'):
             authors.append(data2['name'])
+            #create authors
+            if data2.has_key('birth_date') and data2.has_key('death_date'):
+                data2['birth_date'] = data2['birth_date'].strip().strip('.')
+                data2['death_date'] = data2['death_date'].strip().strip('.')
+                if re.search(ur"(?im)^\d{4}$", data2['birth_date']) and re.search(ur"(?im)^\d{4}$", data2['death_date']):
+                    authorpage = wikipedia.Page(site, data2['name'])
+                    if not authorpage.exists():
+                        wauthorpage = wikipedia.Page(wikipedia.Site('es', 'wikipedia'), data2['name'])
+                        while wauthorpage.exists() and wauthorpage.isRedirectPage():
+                            wauthorpage = wauthorpage.getRedirectTarget()
+                        if wikipedia.Page(site, wauthorpage.title()).exists():
+                            red = u"#REDIRECT [[%s]]" % (wauthorpage.title())
+                            authorpage.put(red, red)
+                            continue
+                        ocupacion = []
+                        nacionalidad = '' #no siempre la nacionalidad indicada es donde se nace, pero...
+                        if wauthorpage.exists():
+                            if re.search(ur"(?im)Nacidos en %s" % (data2['birth_date']), wauthorpage.get()) and re.search(ur"(?im)Fallecidos en %s" % (data2['death_date']), wauthorpage.get()):
+                                if re.search(ur"(?im)\:\s*Escritores (de|en)", wauthorpage.get()):
+                                    ocupacion.append('escritor')
+                                if re.search(ur"(?im)\:\s*Juristas (de|en)", wauthorpage.get()):
+                                    ocupacion.append('jurista')
+                                if re.search(ur"(?im)\:\s*Abogados (de|en)", wauthorpage.get()):
+                                    ocupacion.append('abogado')
+                                if re.search(ur"(?im)\:\s*Dramaturgos (de|en)", wauthorpage.get()):
+                                    ocupacion.append('dramaturgo')
+                                if re.search(ur"(?im)\:\s*Poetas (de|en)", wauthorpage.get()):
+                                    ocupacion.append('poeta')
+                                if re.search(ur"(?im)\:\s*Bibliógrafos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'bibliógrafo')
+                                if re.search(ur"(?im)\:\s*Filósofos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'filósofo')
+                                if re.search(ur"(?im)\:\s*Matemáticos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'matemático')
+                                if re.search(ur"(?im)\:\s*Teólogos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'teólogo')
+                                if re.search(ur"(?im)\:\s*Historiadores (de|en)", wauthorpage.get()):
+                                    ocupacion.append('historiador')
+                                if re.search(ur"(?im)\:\s*Políticos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'político')
+                                if re.search(ur"(?im)\:\s*Filólogos (de|en)", wauthorpage.get()):
+                                    ocupacion.append(u'filólogo')
+                                m = re.findall(ur"(?im)(?:Escritores|Juristas|Abogados|Dramaturgos|Poetas|Filósofos|Bibliógrafos|Matemáticos|Teólogos|Historiadores|Políticos|Filólogos) (?:de|en) (Alemania|Austria|Chile|Colombia|Cuba|Ecuador|El Salvador|España|Estados Unidos|Finlandia|Francia|Grecia|Guatemala|Honduras|India|Italia|Nicaragua|Paraguay|Perú|Polonia|Portugal|Puerto Rico|Rumanía|Rusia|Suecia|Suiza)", wauthorpage.get())
+                                if m:
+                                    sett = set(m)
+                                    if len(sett) == 1:
+                                        nacionalidad = sett.pop()
+                        else:
+                            pass #no tenemos datos de ocupación ni nacionalidad...
+                        output = u"""{{Infobox Persona
+|nombre=%s
+|apellidos=%s
+|imagen=
+|fechanacimiento=%s
+|lugarnacimiento=%s
+|fechafallecimiento=%s
+|lugarfallecimiento=
+|ocupación=%s
+}}
+
+{{obras por autor}}%s""" % (data2['name'].split(' ')[0], u' '.join(data2['name'].split(' ')[1:]), data2['birth_date'], nacionalidad, data2['death_date'], u', '.join(ocupacion), wauthorpage.exists() and u'\n\n== Enlaces externos ==\n* {{w|es|%s}}' % (wauthorpage.title()) or '')
+                        print output
+                        authorpage.put(output, output)
+                    
         else: #break
             authors = []
             break
     if not authors:
         continue
+    
     publish_date = data['publish_date']
     ocaid = data['ocaid']
     
