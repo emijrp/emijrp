a7337fe3cfbbc4fa96d1088ee0638c0fbe57764d
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2012-04-06 10:37:28 +0000

    wikiindex creation bot, names and weird chars

--- wikiindex/wikiindex-creation.py
+++ wikiindex/wikiindex-creation.py
@@ -26,7 +26,7 @@ s = wikipedia.Site('wikiindex', 'wikiindex')
 #cat = catlib.Category(s, 'Category:MediaWiki')
 
 cats = { 'wikia.com': '{{wikia}}', }
-langs = {'de': 'German', 'en': 'English', 'en-gb': 'English', 'es': 'Spanish', 'fr': 'French', 'it': 'Italian', 'pl': 'Polish', 'pt-br': 'Brazilian Portuguese', 'ru': 'Russian'}
+langs = {'de': 'German', 'en': 'English', 'en-gb': 'English', 'es': 'Spanish', 'fi': 'Finnish', 'fr': 'French', 'it': 'Italian', 'pl': 'Polish', 'pt-br': 'Brazilian Portuguese', 'ru': 'Russian', 'zh': 'Chinese', 'zh-cn': 'Chinese', 'zh-tw': 'Taiwanese Mandarin', }
 start = ''
 if len(sys.argv) == 2:
     start = sys.argv[1]
@@ -58,10 +58,12 @@ for wiki in wikis:
     
     name = u''
     try:
-        name = re.findall(ur"wgSiteName=\"([^\"]+)\"", raw)[0]
+        name = re.findall(ur"(?im)wgSiteName=\"([^\n\r]+)\",\n", raw)[0]
     except:
         print 'ERROR in name'
         continue
+    name = re.sub(ur"\x26", "&", name)
+    name = re.sub(ur"\\", "", name)
     logo = u'[[Image:NoLogo.png]]'
     if re.search(ur'<div id="p-logo"><a style="background-image: url\((/w/skins/[^\)]+)\);', raw):
         logo = u"%s%s" % (wiki, re.findall(ur'<div id="p-logo"><a style="background-image: url\((/w/skins/[^\)]+)\);', raw)[0])
