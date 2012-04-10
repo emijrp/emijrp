dd245d7484af5c3066d66801acb4cb3a0529cd5c
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2012-04-10 10:57:50 +0000

    langs

--- wikiindex/wikiindex-creation.py
+++ wikiindex/wikiindex-creation.py
@@ -26,7 +26,7 @@ s = wikipedia.Site('wikiindex', 'wikiindex')
 #cat = catlib.Category(s, 'Category:MediaWiki')
 
 cats = { 'wikia.com': '{{wikia}}', }
-langs = {'ace': 'Acehnese', 'af': 'Afrikaans', 'aln': 'Gheg Albanian', 'am': 'Amharic', 'an': 'Aragonese', 'ang': 'Old English', 'ar': 'Arabic', 'arz': 'Egyptian Arabic', 'az': 'Azerbaijani', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'en-gb': 'English', 'es': 'Spanish', 'fi': 'Finnish', 'fr': 'French', 'hu': 'Hungarian', 'id': 'Indonesian', 'it': 'Italian', 'ka': 'Georgian', 'nl': 'Dutch', 'pl': 'Polish', 'pt': 'Portuguese', 'pt-br': 'Brazilian Portuguese', 'ru': 'Russian', 'sk': 'Slovak', 'sq': 'Albanian', 'sv': 'Swedish', 'tr': 'Turkish', 'ty': 'Tahitian', 'zh': 'Chinese', 'zh-cn': 'Chinese', 'zh-tw': 'Taiwanese Mandarin', }
+langs = {'ace': 'Acehnese', 'af': 'Afrikaans', 'aln': 'Gheg Albanian', 'am': 'Amharic', 'an': 'Aragonese', 'ang': 'Old English', 'ar': 'Arabic', 'arz': 'Egyptian Arabic', 'az': 'Azerbaijani', 'bat-smg': 'Samogitian', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'en-gb': 'English', 'es': 'Spanish', 'fi': 'Finnish', 'fr': 'French', 'hu': 'Hungarian', 'id': 'Indonesian', 'it': 'Italian', 'ka': 'Georgian', 'nl': 'Dutch', 'pl': 'Polish', 'pt': 'Portuguese', 'pt-br': 'Brazilian Portuguese', 'ru': 'Russian', 'sk': 'Slovak', 'sq': 'Albanian', 'sv': 'Swedish', 'tr': 'Turkish', 'ty': 'Tahitian', 'zh': 'Chinese', 'zh-cn': 'Chinese', 'zh-tw': 'Taiwanese Mandarin', }
 start = ''
 if len(sys.argv) == 2:
     start = sys.argv[1]
