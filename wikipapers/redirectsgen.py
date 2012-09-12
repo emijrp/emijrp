d1704b4a8324cdf1c1fc086be4652a551e34c0f5
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2012-09-12 09:18:00 +0000

    redirects gen

--- wikipapers/redirectsgen.py
+++ wikipapers/redirectsgen.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2011-2012 emijrp
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import sys
 
@@ -11,11 +25,11 @@ import unicodedata
 def removeaccute(s):
     return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
-def remove1(s):
-    s = re.sub(ur"[\.\:\;\,\"]", ur"", s)
+def remove1(s): #replace with no-space
+    s = re.sub(ur"[\.\:\;\,\"\!\¡\«\»]", ur"", s)
     return s
 
-def remove2(s):
+def remove2(s): #replace with a single space
     s = re.sub(ur"[\-\–]", ur" ", s)
     return s
 
@@ -23,53 +37,47 @@ def unquote(s):
     s = re.sub(ur"&#34;", ur'"', s)
     return s
 
-skip = u''
-site = ''
-site = wikipedia.Site(sys.argv[1], sys.argv[1])
-if len(sys.argv) > 2:
-    skip = sys.argv[2]
-gen = pagegenerators.AllpagesPageGenerator(start = skip, namespace=0, site=site)
-pre = pagegenerators.PreloadingGenerator(gen)
-for page in pre:
-    if not page.exists():
-        continue
-    wtitle = page.title()
-    
-    print wtitle
-    if page.isRedirectPage():
-        tar = page.getRedirectTarget()
-        if tar.exists():
-            target = tar.title()
-        else:
-            continue
+def main():
+    skip = u''
+    if len(sys.argv) > 1:
+        site = wikipedia.Site(sys.argv[1], sys.argv[1])
     else:
-        target = wtitle
-    
-    if len(wtitle) > 1:
-        wtitle_ = wtitle[0]+wtitle[1:].lower()
-        redirects = set()
-        redirects.add(wtitle)
-        redirects.add(remove1(wtitle))
-        redirects.add(remove2(wtitle))
-        redirects.add(removeaccute(wtitle))
-        redirects.add(remove1(remove2(wtitle)))
-        redirects.add(remove1(removeaccute(wtitle)))
-        redirects.add(remove2(removeaccute(wtitle)))
-        redirects.add(remove1(remove2(removeaccute(wtitle))))
-        redirects.add(wtitle_)
-        redirects.add(remove1(wtitle_))
-        redirects.add(remove2(wtitle_))
-        redirects.add(removeaccute(wtitle_))
-        redirects.add(remove1(remove2(wtitle_)))
-        redirects.add(remove1(removeaccute(wtitle_)))
-        redirects.add(remove2(removeaccute(wtitle_)))
-        redirects.add(remove1(remove2(removeaccute(wtitle_))))
+        print 'python script.py wikifamily [skiptopage]'
+        sys.exit()
+    if len(sys.argv) > 2:
+        skip = sys.argv[2]
+    gen = pagegenerators.AllpagesPageGenerator(start=skip, namespace=0, site=site)
+    pre = pagegenerators.PreloadingGenerator(gen)
+    alltitles = []
+    for page in pre:
+        if not page.exists() or page.isRedirectPage():
+            continue
+        alltitles.append(page.title())
+        print page.title()
         
-        print redirects
-        for redirect in redirects:
-            redirect = redirect.strip()
-            if redirect and redirect != wtitle:
-                red = wikipedia.Page(site, redirect)
-                if not red.exists():
-                    output = u"#REDIRECT [[%s]]" % (target)
-                    red.put(output, output)
+    for wtitle in alltitles:
+        if len(wtitle) > 1:
+            wtitle_ = wtitle[0]+wtitle[1:].lower()
+            redirects = set()
+            for t in [wtitle, wtitle_]:
+                redirects.add(t)
+                redirects.add(remove1(t))
+                redirects.add(remove2(t))
+                redirects.add(removeaccute(t))
+                redirects.add(remove1(remove2(t)))
+                redirects.add(remove1(removeaccute(t)))
+                redirects.add(remove2(removeaccute(t)))
+                redirects.add(remove1(remove2(removeaccute(t))))
+            
+            print redirects
+            for redirect in redirects:
+                redirect = redirect.strip()
+                if redirect and redirect != wtitle and not redirect in alltitles:
+                    red = wikipedia.Page(site, redirect)
+                    if not red.exists():
+                        output = u"#REDIRECT [[%s]]" % (wtitle)
+                        msg = u"BOT - Creating redirect to [[%s]]" % (wtitle)
+                        red.put(output, msg)
+
+if __name__ == '__main__':
+    main()
